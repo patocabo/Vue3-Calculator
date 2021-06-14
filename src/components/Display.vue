c806f854c8179e42678c4bc3724e81840166fe6c
Patricio Cabo <pato.mdp@gmail.com>
2021-06-14 13:12:34 -0300

    first commit

---
+++ src/components/Display.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Display"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Memory.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Memory"
+}
+</script>
+
+<style scoped>
+
+</style>
